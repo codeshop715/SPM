06232a57417dd0b84e21c4e54372d8d9319a875d
code_house <74033414+codeshop715@users.noreply.github.com>
2023-08-13 18:22:32 +0800

    code

---
+++ scripts/convert_tfrecord_to_h5.py
@@ -0,0 +1,96 @@
+import os
+import datetime
+import numpy as np
+import time
+import torch
+import h5py as h5
+import sys,os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # __file__获取执行文件相对路径，整行为取上一级的上一级目录
+sys.path.append(BASE_DIR)
+from tqdm import tqdm
+from pathlib import Path
+
+#import utils.deit_util as utils
+
+from datasets.meta_dataset import reader
+#from datasets.meta_dataset.utils import parse_record
+import cv2
+import torchvision.transforms as transforms
+from PIL import Image
+
+from datasets.meta_dataset.utils import Split
+from datasets.meta_dataset import dataset_spec as dataset_spec_lib
+
+
+def convert_class_datasets(dataset_spec_list):
+    for i in range(len(dataset_spec_list)):
+
+        # each class of every source/dataset is stored in a tfrecord -> TFRecordDataset
+        class_datasets = []
+
+        episode_reader = reader.Reader(dataset_spec=dataset_spec_list[i],
+                                       split=Split.TRAIN,
+                                       shuffle=True)
+        class_datasets += episode_reader.construct_class_datasets()
+
+        episode_reader = reader.Reader(dataset_spec=dataset_spec_list[i],
+                                       split=Split.VALID,
+                                       shuffle=True)
+        class_datasets += episode_reader.construct_class_datasets()
+
+        episode_reader = reader.Reader(dataset_spec=dataset_spec_list[i],
+                                       split=Split.TEST,
+                                       shuffle=True)
+        class_datasets += episode_reader.construct_class_datasets()
+
+        print(f'==> Converting {dataset_spec_list[i].name} of {len(class_datasets)} classes.')
+
+        if dataset_spec_list[i].name == 'quickdraw':
+            resizer = transforms.Resize((28, 28))
+        else:
+            resizer = transforms.Resize((224, 224))
+
+        # h5 writing
+        #h5_file = dataset_spec_list[i].path + '.h5'
+        for cls_dset in class_datasets:
+            h5_file = cls_dset.data_path.replace('tfrecords', 'h5')
+            print(f'* Writing {h5_file}:')
+            with h5.File(h5_file, 'w') as f:
+                for j, feat_dic in enumerate(tqdm(cls_dset)):
+                    grp = f.create_group(f'{j}')
+                    image = feat_dic["image"]
+                    image = cv2.imdecode(image, -1)
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # from BGR to RGB
+
+                    # due to storage limit, we store resized images if original is larger than 224
+                    # for QuickDraw, we use 28x28 as this already yields an h5 file with 218GB
+                    if (image.shape[0] >= 224 and image.shape[1] >= 224) or dataset_spec_list[i].name == 'quickdraw':
+                        image = Image.fromarray(image)
+                        image = resizer(image)
+                        image = np.array(image)
+
+                    grp.create_dataset('image', data=image)
+                    grp.create_dataset('label', data=np.array(feat_dic['label']))
+                    grp.create_dataset('id', data=np.array(feat_dic['id']))
+                    #if j % 10 == 0:
+                    #    print(f'Ep{j}: img.shape={image.shape}')
+
+
+def main(data_path='/path/to/meta-dataset/tf_records'):
+    ###########################################################################
+    # EDIT here if you don't want to convert all domains
+    domains = ['ilsvrc_2012', 'omniglot', 'aircraft', 'cu_birds', 'dtd', 'quickdraw', 'fungi', 'vgg_flower', 'traffic_sign', 'mscoco']
+    ###########################################################################
+
+    # Conversion
+    all_dataset_specs = []
+    for dname in domains:
+        dataset_records_path = os.path.join(data_path, dname)
+        dataset_spec = dataset_spec_lib.load_dataset_spec(dataset_records_path)
+        all_dataset_specs.append(dataset_spec)
+
+    convert_class_datasets(all_dataset_specs)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
---
+++ test_bscdfsl.py
@@ -0,0 +1,116 @@
+import os
+import numpy as np
+import time
+import random
+import torch
+import torch.backends.cudnn as cudnn
+import json
+
+from pathlib import Path
+from tabulate import tabulate
+
+from engine import evaluate
+import utils.deit_util as utils
+from utils.args import get_args_parser
+from models import get_model
+from datasets import get_bscd_loader
+
+
+def main(args):
+    args.distributed = False # CDFSL dataloader doesn't support DDP
+    args.eval = True
+
+    print(args)
+    device = torch.device(args.device)
+
+    cudnn.benchmark = True
+
+    ##############################################
+    # Model
+    print(f"Creating model: {args.deploy} {args.arch}")
+
+    model = get_model(args)
+    model.to(device)
+
+    if args.resume:
+        checkpoint = torch.load(args.resume, map_location='cpu')
+        model.load_state_dict(checkpoint['model'], strict=False)
+        # model.load_state_dict(checkpoint, strict=True)
+        print(f'Load ckpt from {args.resume}')
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('number of params:', n_parameters)
+
+    ##############################################
+    # Test
+    criterion = torch.nn.CrossEntropyLoss()
+    datasets = args.cdfsl_domains
+    var_accs = {}
+
+    for domain in datasets:
+        print(f'\n# Testing {domain} starts...\n')
+
+        data_loader_val = get_bscd_loader(domain, args.test_n_way, args.n_shot, args.image_size)
+
+        # validate lr
+        best_lr = args.ada_lr
+        if args.deploy == 'finetune':
+            print("Start selecting the best lr...")
+            best_acc = 0
+            for lr in [0, 0.0001, 0.0005, 0.001]:
+                model.lr = lr
+                test_stats = evaluate(data_loader_val, model, criterion, device, seed=1234, ep=5)
+                acc = test_stats['acc1']
+                print(f"*lr = {lr}: acc1 = {acc}")
+                if acc > best_acc:
+                    best_acc = acc
+                    best_lr = lr
+            model.lr = best_lr
+            print(f"### Selected lr = {best_lr}")
+
+        # final classification
+        data_loader_val.generator.manual_seed(args.seed + 10000)
+        test_stats = evaluate(data_loader_val, model, criterion, device)
+        var_accs[domain] = (test_stats['acc1'], test_stats['acc_std'], best_lr)
+
+        print(f"{domain}: acc1 on {len(data_loader_val.dataset)} test images: {test_stats['acc1']:.1f}%")
+
+        if args.output_dir and utils.is_main_process():
+            test_stats['domain'] = domain
+            test_stats['lr'] = best_lr
+            with (output_dir / f"log_test_{args.deploy}_{args.train_tag}.txt").open("a") as f:
+                f.write(json.dumps(test_stats) + "\n")
+
+    # print results as a table
+    if utils.is_main_process():
+        rows = []
+        for dataset_name in datasets:
+            row = [dataset_name]
+            acc, std, lr = var_accs[dataset_name]
+            conf = (1.96 * std) / np.sqrt(len(data_loader_val.dataset))
+            row.append(f"{acc:0.2f} +- {conf:0.2f}")
+            row.append(f"{lr}")
+            rows.append(row)
+        np.save(os.path.join(output_dir, f'test_results_{args.deploy}_{args.train_tag}.npy'), {'rows': rows})
+
+        table = tabulate(rows, headers=['Domain', args.arch, 'lr'], floatfmt=".2f")
+        print(table)
+        print("\n")
+
+        if args.output_dir:
+            with (output_dir / f"log_test_{args.deploy}_{args.train_tag}.txt").open("a") as f:
+                f.write(table)
+
+if __name__ == '__main__':
+    parser = get_args_parser()
+    args = parser.parse_args()
+    args.train_tag = 'pt' if args.resume == '' else 'ep'
+    args.train_tag += f'_step{args.ada_steps}_lr{args.ada_lr}_prob{args.aug_prob}'
+
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    import sys
+    with (output_dir / f"log_test_{args.deploy}_{args.train_tag}.txt").open("a") as f:
+        f.write(" ".join(sys.argv) + "\n")
+
+    main(args)
